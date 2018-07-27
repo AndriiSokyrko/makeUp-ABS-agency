91aa13393439bc0bed2d653bc0ef1e8cbea04d38
sorey14 <sorey14@rambler.ru>
2018-07-27 13:17:38 +0300

    First commit

---
+++ app/index.php
@@ -0,0 +1,270 @@
+﻿<!DOCTYPE html PUBLIC "//-W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml1.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xmlns="http://www.w3.org/1999/html" xmlns="http://www.w3.org/1999/html"
+      xmlns="http://www.w3.org/1999/html">
+<head>
+	<title>title</title>
+	<meta http-equiv="Content-type" content="text/html; charset=utf-8"/>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<link rel="stylesheet" href="css/style.min.css" type="text/css"/>
+	<link rel="stylesheet" href="css/media-queries.min.css" type="text/css"/>
+	<link rel="stylesheet" href="fonts/css/font.css" type="text/css"/>
+	<link rel="shortcut icon" href="img/favicon.jpg" type="image/x-icon"/>
+	<link rel="icon" href="img/favicon.jpg" type="image/x-icon"/>
+	<meta name="description" content="description"/>
+	<meta name="keywords" content="keywords"/>
+	<!--	<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js"></script>-->
+	<link rel="stylesheet" type="text/css" href="libs/bootstrap-grid/bootstrap-grid-3.3.1.min.css"/>
+	<link rel="stylesheet" type="text/css" href="libs/slider/index.css"/>
+	<script type="text/javascript" src="libs/slider/click-carousel.js"></script>
+	<script type="text/javascript" src="libs/jQuery/jquery-1.9.1.min.js"></script>
+	<script type="text/javascript" src="js/external/jquery.movingboxes.js"></script>
+	<script type="text/javascript" src="js/common.min.js"></script>
+
+	<link rel="stylesheet" href="libs/owl-carousel/owl.carousel.css">
+	<link rel="stylesheet" href="libs/owl-carousel/owl.theme.css">
+	<script type="text/javascript" src="libs/owl-carousel/owl.carousel.min.js"></script>
+</head>
+<body>
+
+<header class="header">
+	<div class="header-top-bg">
+
+		<div class="container header-top-container">
+			<div class="row header-top-row">
+
+			<div class="col-xs-6   col-sm-6  col-md-6 col-lg-6 header-top-left">
+				<div class="header-logo">
+					<a href="#"><img src="img/med-logo.jpg" alt=""></a>
+				</div>
+				<div class="header-top-right">
+					<div class="header-top-right-phone">+7 777 777 77 77</div>
+					<a href="#" class="header-top-right-order">Заказать звонок</a>
+				</div>
+			</div><!--  //col-->
+			<div class="col-xs-6   col-sm-6  col-md-6 col-lg-6 header-top-right">
+
+			</div><!--  //col-->
+		</div><!--  //row-->
+	</div><!--  //container-->
+	</div><!--  //header-top-bg-->
+
+
+	<div class="header-info-bg">
+
+		<div class="container header-info-container">
+			<div class="row header-info-row">
+
+				<div class="col-xs-6   col-sm-6  col-md-6 col-lg-6 header-info-left">
+					<div class="header-logo">
+						<a href="#"><img src="img/med-logo.jpg" alt=""></a>
+					</div>
+					<div class="header-info-right">
+						<div class="header-info-right-phone">+7 777 777 77 77</div>
+						<a href="#" class="header-top-right-order">Заказать звонок</a>
+					</div>
+				</div><!--  //col-->
+				<div class="col-xs-6   col-sm-6  col-md-6 col-lg-6 header-info-right">
+
+				</div><!--  //col-->
+			</div><!--  //row-->
+		</div><!--  //container-->
+	</div><!-- //header-info-bg -->
+
+	
+		<div class="header-menu-bg">
+			<div class="container header-menu-container">
+				<div class="row header-menu-row">
+					<ul>
+						<li><a href="#">О компании</a></li>
+						<li><a href="#">Каталог</a></li>
+						<li><a href="#">Услуги</a></li>
+						<li><a href="#">Акции</a></li>
+						<li><a href="#">Статьи</a></li>
+						<li><a href="#">Контакты</a></li>
+					</ul>
+				</div><!--  //row-->
+			</div><!--  //container-->
+		</div><!-- header-menu-bg -->
+
+	<div class="header-slider-bg">
+		<!--		<div class="slider-left-bg"></div>-->
+		<div class="container header-slider-container">
+			<div class="row header-slider-row">
+				<div class="header-slider-left">
+					<div class="left-wrapper">
+						<h1 class="">Телескопические <span>погрузчики МАНИТОУ</span></h1>
+						<div class="clearfix"></div>
+						<div class="descript">
+							Уникальные многофункциональные <br>
+							телескопические погрузчики Манитоу
+							<span>Узнайте на что он способен!</span>
+							<a href="">Подробнее</a>
+						</div><!--  //descript-->
+					</div><!--  //left-wrapper-->
+
+				</div><!-- //left -->
+				<div class="header-slider-right">
+					<form action="" class="form-order">
+						<h2>Оставьте нам заявку<span>И мы с вами свяжемся</span></h2>
+						<input type="text" placeholder="Ваше имя">
+						<input type="text" placeholder="Телефон">
+						<input type="submit" value="Оставить заявку">
+					</form>
+
+
+				</div><!-- //right -->
+			</div><!--  //row-->
+		</div><!--  //container-->
+	</div><!--  //header-slider-bg-->
+</header><!-- //header -->
+<main>
+<section class="sct-2-bg">
+	<div class="container sct-2-container">
+		<div class="row sct-2-row">
+			<div class="col-xs-6   col-sm-6  col-md-6 col-lg-6 sct-1-left">
+			</div><!-- //col -->
+			<div class="col-xs-6   col-sm-6  col-md-6 col-lg-6 sct-1-right">
+			</div> <!-- //col -->
+		</div><!-- //row -->
+	</div><!-- //container -->
+</section><!-- //sct-2 -->
+</main>
+
+
+
+<footer class="footer">
+	<div class="footer-top-bg">
+
+		<div class="container footer-top-container">
+			<div class="row footer-top-row">
+
+				<div class="col-xs-6   col-sm-6  col-md-6 col-lg-6 footer-top-left">
+					<div class="footer-logo">
+						<img src="img/med-logo.jpg" alt="">
+					</div>
+					<div class="footer-top-right">
+						<div class="footer-top-right-phone">+7 777 777 77 77</div>
+						<a href="#" class="footer-top-right-order">Заказать звонок</a>
+					</div>
+				</div><!--  //col-->
+				<div class="col-xs-6   col-sm-6  col-md-6 col-lg-6 footer-top-right">
+
+				</div><!--  //col-->
+			</div><!--  //row-->
+		</div><!--  //container-->
+	</div><!--  //footer-top-bg-->
+</footer>
+
+
+
+<!--ьлок акция  timer -->
+<!--<div class="sct-3-action-time">-->
+<!--	<span class="sct-3-action-title">До конца акции осталось</span>-->
+<!--	<div class="sct-3-timer">-->
+<!--		<div class="sct-3-digital">-->
+<!--			<span id="sct-3-day">22</span><span id="sct-3-hour">16</span><span-->
+<!--				id="sct-3-minute">48</span>-->
+<!--		</div>-->
+<!--		<div class="sct-3-text">-->
+<!--			<span>Дней</span><span>Часов</span><span>Минут</span>-->
+<!--		</div>-->
+<!--	</div>-->
+<!--</div>-->
+
+<!-- блок отзывы клиенов  -->
+<!--<div class="sct-6-otzive">-->
+<!--	<img src="img/otziv.png" alt="">-->
+<!---->
+<!--	<div class="sct-6-itzive-total">-->
+<!--		<div class="sct-6-excerpt">За два месяца перестали выпадть волосы</div>-->
+<!--		<div class="sct-6-description">-->
+<!--			Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab cupiditate, magnam nulla quae-->
+<!--			quo repudiandae tempora ut. Eos, optio quos!-->
+<!--		</div>-->
+<!--	</div>-->
+<!--</div>-->
+
+<!--<div class="owl-demo1">-->
+<!--	<!--				<div class="col-xs-4 col-sm-4 col-md-4 col-lg-4 item ">-->
+<!--	<div class="item">-->
+<!--		<div class="owl-text">-->
+<!--			<h2>Lorem ipsum dolor sit amet.</h2>-->
+<!--			<ul>-->
+<!--				<li class="item-text">Lorem ipsum dolor</li>-->
+<!--				<li class="item-text">Lorem ipsum dolor</li>-->
+<!--				<li class="item-text">Lorem ipsum dolor</li>-->
+<!--				<li class="item-text">Lorem ipsum dolor</li>-->
+<!--				<li class="item-text">Lorem ipsum dolor</li>-->
+<!--			</ul>-->
+<!--		</div>-->
+<!--		<img class="img-responsive owl-img" src="img/main-train-left.png" alt="The Last of us">-->
+<!--	</div>-->
+<!---->
+<!---->
+<!--	<!--				<div class="col-xs-4 col-sm-4 col-md-4 col-lg-4 item ">-->
+<!--	<div class="item">-->
+<!--		<div class="owl-text">-->
+<!--			<h2>Lorem ipsum dolor sit amet.</h2>-->
+<!--			<ul>-->
+<!--				<li class="item-text">Lorem ipsum dolor</li>-->
+<!--				<li class="item-text">Lorem ipsum dolor</li>-->
+<!--				<li class="item-text">Lorem ipsum dolor</li>-->
+<!--				<li class="item-text">Lorem ipsum dolor</li>-->
+<!--				<li class="item-text">Lorem ipsum dolor</li>-->
+<!--			</ul>-->
+<!--		</div>-->
+<!--		<img class="img-responsive owl-img" src="img/main-train-center.png" alt="The Last of us">-->
+<!--	</div>-->
+<!---->
+<!---->
+<!--	<!--				<div class="col-xs-4 col-sm-4 col-md-4 col-lg-4 item">-->
+<!--	<div class="item">-->
+<!--		<div class="owl-text">-->
+<!--			<h2>Lorem ipsum dolor sit amet.</h2>-->
+<!--			<ul>-->
+<!--				<li class="item-text">Lorem ipsum dolor</li>-->
+<!--				<li class="item-text">Lorem ipsum dolor</li>-->
+<!--				<li class="item-text">Lorem ipsum dolor</li>-->
+<!--			</ul>-->
+<!--		</div>-->
+<!--		<img class="img-responsive owl-img" src="img/main-train-right.png" alt="The Last of us">-->
+<!--	</div>-->
+<!---->
+<!---->
+<!--	<!--				<div class="col-xs-4 col-sm-4 col-md-4 col-lg-4 item">-->
+<!--	<div class="item">-->
+<!--		<div class="owl-text">-->
+<!--			<h2>Lorem ipsum dolor sit amet.</h2>-->
+<!--			<ul>-->
+<!--				<li class="item-text">Lorem ipsum dolor</li>-->
+<!--				<li class="item-text">Lorem ipsum dolor</li>-->
+<!--				<li class="item-text">Lorem ipsum dolor</li>-->
+<!--				<li class="item-text">Lorem ipsum dolor</li>-->
+<!--			</ul>-->
+<!--		</div>-->
+<!--		<img class="img-responsive owl-img" src="img/main-train-right.png" alt="The Last of us">-->
+<!--	</div>-->
+<!--	<!--				<div class="col-xs-4 col-sm-4 col-md-4 col-lg-4 item">-->
+<!--	<div class="item">-->
+<!--		<div class="owl-text">-->
+<!--			<h2>Lorem ipsum dolor sit amet.</h2>-->
+<!--			<ul>-->
+<!--				<li class="item-text">Lorem ipsum dolor</li>-->
+<!--				<li class="item-text">Lorem ipsum dolor</li>-->
+<!--				<li class="item-text">Lorem ipsum dolor</li>-->
+<!--				<li class="item-text">Lorem ipsum dolor</li>-->
+<!--				<li class="item-text">Lorem ipsum dolor</li>-->
+<!---->
+<!--			</ul>-->
+<!--		</div>-->
+<!--		<img class="img-responsive owl-img" src="img/main-train-right.png" alt="The Last of us">-->
+<!--	</div>-->
+<!---->
+<!--</div><!-- //owl-demo1 -->
+<!--<div class="owl-nav">-->
+<!--	<div class="owl-prev"><div class="icomoon-chevron-left"></div></div>-->
+<!--	<div class="owl-next"><div class="icomoon-chevron-right"></div></div>-->
+<!--</div><!--  //owl-naw -->
+</body>
+</html>
